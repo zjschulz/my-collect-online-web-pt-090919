49b0f64782bd2918366e364600dbecc5cf153282
Zachary Schulz <zjschulz@gmail.com>
2019-09-20 22:25:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,3 @@
 def my_collect(array)
-  if block_given?
-    i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
- 
-    array
-  else
-    print "This block should not run!"
-  end
 end
 
